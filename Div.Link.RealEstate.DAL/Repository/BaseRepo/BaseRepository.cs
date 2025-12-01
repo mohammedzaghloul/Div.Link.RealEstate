d3e7221c24df76e1615ed6a41c12f1cfcd197545
mohammedzaghloul <mohammedzaghloul123@gmail.com>
2025-12-01 17:55:21 +0200

    Initial commit: Complete Real Estate project with ASP.NET

---
+++ Div.Link.RealEstate.DAL/Repository/BaseRepo/BaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Div.Link.RealEstate.DAL.Repository.BaseRepo
+{
+    internal class BaseRepository
+    {
+    }
+}
