6a0d37a5a62635a47c4421504f6dce29656b5db3
mohammedzaghloul <mohammedzaghloul123@gmail.com>
2025-12-01 17:55:21 +0200

    Initial commit: Complete Real Estate project with ASP.NET

---
+++ Div.Link.RealEstate.DAL/Model/ApplicationUser/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Div.Link.RealEstate.DAL.Model.ApplicationUser
+{
+    internal class User
+    {
+    }
+}
